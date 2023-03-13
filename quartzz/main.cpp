f0e9f5e05102731ea5338bbee752ca6fab8a6436
wijiler <josiahosgood@gmail.com>
2023-03-13 09:59:17 -0400

    Initial Commit

---
+++ quartzz/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  quartzz
+//
+//  Created by Wijmac on 3/13/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
